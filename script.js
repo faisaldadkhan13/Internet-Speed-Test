acb9360b61de2e277a3fde040493861b41ae847d
Faisal Dad Khan <54923395+faisaldadkhan13@users.noreply.github.com>
2023-12-25 12:04:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function runSpeedTest() {
     resultElement.textContent = 'Running speed test...';
 
     const startTime = new Date().getTime();
-    const response = await fetch('https://www.example.com'); // Replace with an actual endpoint
+    const response = await fetch('https://www.google.com'); // Replace with an actual endpoint
 
     if (response.ok) {
         const endTime = new Date().getTime();
